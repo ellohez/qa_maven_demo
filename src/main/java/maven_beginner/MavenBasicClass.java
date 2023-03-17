124a322550470c941ddd21cc244ee70c113737aa
ellohez <helenpjobs@gmail.com>
2023-03-17 10:42:21 +0000

    test

--- src/main/java/maven_beginner/MavenBasicClass.java
+++ src/main/java/maven_beginner/MavenBasicClass.java
@@ -1,10 +1,10 @@
 package maven_beginner;
 
 public class MavenBasicClass {
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World!");
+
+		System.out.println("Hello, World!");
 	}
-				
+
 }
